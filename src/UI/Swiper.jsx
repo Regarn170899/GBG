2f09d011826a67b797fda611e5358008a2970d88
Regarn170899 <1526497@gmail.com>
2023-01-28 17:17:31 +0300

    Final Swiper fix
    Final Swiper fix

--- src/UI/Swiper.jsx
+++ src/UI/Swiper.jsx
@@ -1,48 +1,58 @@
-import React, { useRef, useState } from "react";
+import React, {useRef, useState} from "react";
 // Import Swiper React components
-import { Swiper, SwiperSlide } from "swiper/react";
+import {Swiper, SwiperSlide} from "swiper/react";
 import styles from "./Swiper.module.css";
 // Import Swiper styles
 import "swiper/css";
 import "swiper/css/pagination";
 import "swiper/css/navigation";
 
+
+import "./Swiper.css";
 // import required modules
-import { Pagination, Navigation } from "swiper";
+import {Navigation} from "swiper";
 
 export default function Slider() {
-  const [swiperRef, setSwiperRef] = useState(null);
+    const [swiperRef, setSwiperRef] = useState(null);
 
-  return (
-    <div className={styles.swiperContainer}>
-      <Swiper
-        slidesPerView={3}
-        centeredSlides={true}
-        spaceBetween={20}
-        pagination={{
-          type: "fraction",
-        }}
-        style={{ height: "800px" }}
-        navigation={true}
-        modules={[Navigation]}
-        className="mySwiper"
-        direction={"vertical"}
-      >
-        <div className={styles.slideContainer}>
-          <SwiperSlide>
-            <div className={styles.swiperWrapper}></div>
-          </SwiperSlide>
-          <SwiperSlide>
-            <div className={styles.swiperWrapper}></div>
-          </SwiperSlide>
-          <SwiperSlide>
-            <div className={styles.swiperWrapper}></div>
-          </SwiperSlide>
-          <SwiperSlide>
-            <div className={styles.swiperWrapper}></div>
-          </SwiperSlide>
+    return (
+        <div className={styles.swiperContainer}>
+            <Swiper
+                effect={"creative"}
+                grabCursor={true}
+                slidesPerView={3}
+                centeredSlides={true}
+                spaceBetween={20}
+                pagination={{
+                    type: "fraction",
+                }}
+                style={{height: "800px" }}
+                navigation={true}
+                modules={[Navigation]}
+                className="mySwiper"
+                direction={"vertical"}
+            >
+                <div className={styles.swiperContainer}>
+                    <div className={styles.slideContainer}>
+                        <SwiperSlide>
+                            <div className={styles.swiperWrapper}></div>
+                        </SwiperSlide>
+                        <SwiperSlide>
+                            <div className={styles.swiperWrapper}></div>
+                        </SwiperSlide>
+                        <SwiperSlide>
+                            <div className={styles.swiperWrapper}></div>
+                        </SwiperSlide>
+                        <SwiperSlide>
+                            <div className={styles.swiperWrapper}></div>
+                        </SwiperSlide>
+                    </div>
+                    <div className={styles.buttonNext}></div>
+                    <div className={styles.buttonPre}></div>
+                </div>
+
+
+            </Swiper>
         </div>
-      </Swiper>
-    </div>
-  );
+    );
 }
